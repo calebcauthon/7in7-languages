e3842e5b5d2aed0acf7afa61b1595e542afa285c
Caleb Cauthon <calebcauthon@gmail.com>
2013-12-28 20:46:06 -0600

    Added my first prolog file.

---
+++ prolog/friends.pl
@@ -0,0 +1,16 @@
+%% ---
+%%  Excerpted from "Seven Languages in Seven Weeks",
+%%  published by The Pragmatic Bookshelf.
+%%  Copyrights apply to this code. It may not be used to create training material, 
+%%  courses, books, articles, and the like. Contact us if you are in doubt.
+%%  We make no guarantees that this code is fit for any purpose. 
+%%  Visit http://www.pragmaticprogrammer.com/titles/btlang for more book information.
+%%---
+likes(wallace, cheese).
+likes(grommit, cheese).
+likes(wendolene, sheep).
+hates(wallace, darts).
+loves(grommit, darts).
+
+friend(X, Y) :- \+(X = Y), likes(X, Z), likes(Y, Z).
+enemy(X, Y) :- \+(x = Y), hates(X, Z), loves(Y, Z).
